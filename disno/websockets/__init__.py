e775fca294cd097ee610d99e5879db97a518b256
Middledot <middledot.productions@gmail.com>
2022-02-15 07:48:00 -0500

    Implement websocket reconnects

--- disno/websockets/__init__.py
+++ disno/websockets/__init__.py
@@ -71,7 +71,7 @@ class Heartbeat(threading.Thread):
         self.ev.set()
 
 
-class Websocket:
+class BaseWebsocket:
     def __init__(self, bot_token: str, *, session: aiohttp.ClientSession = None, loop = None):
         user_agent = 'DiscordBot (https://github.com/QwireDev/disno {0}) Python/{1[0]}.{1[1]} aiohttp/{2}'
         self.user_agent = user_agent.format("1.0.0a.1", sys.version_info, aiohttp.__version__)
@@ -94,27 +94,15 @@ class Websocket:
         self.heart = None
         self.sequence = None
         self.session_id = None
-        
+
         self.thread_id = threading.get_ident()
 
         self._buffer = bytearray()
         self._inflator = zlib.decompressobj()
 
-    @classmethod
-    async def initialize(cls, *args, **kwargs):
-        ws = cls(*args, **kwargs)
-
-        ws.gateway = "wss://gateway.discord.gg?encoding='json'&v=9&compress=zlib-stream" # TODO: uhh
-        ws.socket = await ws.connect()
-
-        await ws.poll_receive()
-        await ws.identify()
-
-        return ws
-    
     async def send(self, data):
         await self.socket.send_str(data)
-    
+
     async def send_json(self, data):
         print("[SENT]     ", data)
         await self.socket.send_str(to_json(data))
@@ -124,7 +112,7 @@ class Websocket:
         await self.send_json(self.heartbeat.get_payload())
         self.heartbeat.start()
 
-    async def identify(self):
+    async def identify_payload(self):
         package = {
             "op": OPType.identify,
             "d": {
@@ -146,6 +134,18 @@ class Websocket:
 
         await self.send_json(package)
 
+    async def resume_payload(self):
+        resume_payload = {
+            "op": 6,
+            "d": {
+                "token": self.token,
+                "session_id": self.session_id,
+                "seq": self.sequence
+            }
+        }
+
+        await self.send_json(resume_payload)
+
     async def connect(self):
         kwargs = {
             'max_msg_size': 0,
@@ -157,20 +157,8 @@ class Websocket:
         }
 
         return await self.session.ws_connect(self.gateway, **kwargs)
-    
+
     async def process_receive(self, msg):
-        if type(msg.data) is bytes:
-            self._buffer.extend(msg.data)
-
-            # check if the last four bytes are equal to ZLIB_SUFFIX
-            if len(msg.data) < 4 or msg.data[-4:] != b'\x00\x00\xff\xff':
-                return
-
-            # if the message *does* end with ZLIB_SUFFIX,
-            # get the full message by decompressing the buffers
-            # NOTE: the message is utf-8 encoded.
-            msg = self._inflator.decompress(self._buffer)
-            self._buffer = bytearray()
         try:
             data = from_json(msg)
         except:
@@ -189,21 +177,66 @@ class Websocket:
             payload = data.get('d')
             self.session_id = payload.get('session_id')
 
-        if op == OPType.reconnect:
-            # TODO: Test if this works, I haven't gotten a reconnect yet
-            await self.socket.close()
-            self.socket = await self.connect()
-            resume_payload = {
-                "op": 6,
-                "d": {
-                    "token": self.token,
-                    "session_id": self.session_id,
-                    "seq": self.sequence
-                }
-            }
-            await self.send_json(resume_payload)
-            self.heart = Heartbeat(ws=self, interval=self.heart.interval)
+        return op, event
 
     async def poll_receive(self):
         msg = await self.socket.receive(timeout=120)
+        if type(msg.data) is bytes:
+            self._buffer.extend(msg.data)
+
+            # check if the last four bytes are equal to ZLIB_SUFFIX
+            if len(msg.data) < 4 or msg.data[-4:] != b'\x00\x00\xff\xff':
+                return
+
+            # if the message *does* end with ZLIB_SUFFIX,
+            # get the full message by decompressing the buffers
+            # NOTE: the message is utf-8 encoded.
+            msg = self._inflator.decompress(self._buffer)
+            self._buffer = bytearray()
         await self.process_receive(msg)
+
+
+class Websocket:
+    def __init__(self, bot_token: str, *, session: aiohttp.ClientSession = None, loop = None):
+        super().__init__(bot_token, session=session, loop=loop)
+
+    @classmethod
+    async def initialize(cls, *args, **kwargs):
+        ws = cls(*args, **kwargs)
+
+        ws.gateway = "wss://gateway.discord.gg?encoding='json'&v=9&compress=zlib-stream" # TODO: uhh
+        ws.socket = await ws.connect()
+
+        await ws.poll_receive()
+        await ws.identify_payload()
+
+        return ws
+
+    async def process_receive(self, msg):
+        try:
+            data = from_json(msg)
+        except:
+            print(type(msg))
+            print(msg)
+
+        if data["s"] is not None:
+            self.sequence = data["s"]
+
+        op = data["op"]
+        event = data["t"]
+        print("[RECEIVED] ", data)
+        if op == OPType.hello:
+            await self.ack_hello(data)
+        elif op == OPType.heartbeat:
+            await self.send_json(self.heartbeat.get_payload())
+        elif op == OPType.dispatch and event == 'READY':
+            payload = data.get('d')
+            self.session_id = payload.get('session_id')
+
+        if op == OPType.reconnect:
+            # TODO: Test if this works, I haven't gotten a reconnect yet
+            self.heart.stop()
+            await self.socket.close()
+            self.socket = await self.connect()
+            await self.resume_payload()
+            self.heart = Heartbeat(ws=self, interval=self.heart.interval)
